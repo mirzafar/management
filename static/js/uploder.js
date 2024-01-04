ff6cc0ff78e2563b8201e4c450f69491fd60cc79
mirzafar <m_mirzafar@mail.ru>
2024-01-04 16:16:14 +0600

    f

--- static/js/uploder.js
+++ static/js/uploder.js
@@ -1,19 +1,24 @@
-function uploadImage(sId){
-    return new Promise(function(resolve, reject){
-        var formData = new FormData();
-        formData.append("file", $('#'+sId).prop('files')[0]);
-        console.log(formData)
-        var url = "/upload/";
-        $.ajax({
-           type: "POST",
-           url: url,
-           data: formData,
-           processData: false,
-           contentType: false
-        }).done(function(data){
-            resolve(data);
-        }).fail(function(data){
-            reject(data);
-        });
+function uploadImage(sId, sSize) {
+    return new Promise(async function (resolve, reject) {
+        let formData = new FormData();
+        let file = $('#' + sId).prop('files')[0]
+        formData.append("file", file);
+
+        if (file.size && file.size < (sSize || 10000000)) {
+            try {
+                let response = await fetch('/upload/', {
+                    method: 'POST',
+                    body: formData
+                });
+                resolve(response.json());
+            } catch (error) {
+                reject(error);
+            }
+        } else {
+            resolve({
+                '_message': false,
+                'message': 'Limit file: 10 Mb'
+            })
+        }
     });
 }
