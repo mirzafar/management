e6f78287eaddd654bc97ba6d91655f6e36449336
mirzafar <m_mirzafar@mail.ru>
2024-01-16 23:20:18 +0600

    f

--- server/server.py
+++ server/server.py
@@ -60,6 +60,6 @@ app.static('/static', os.path.join(settings.get('file_path'), 'static'))
 
 if __name__ == '__main__':
     try:
-        app.run('127.0.0.1', port=8102, access_log=False)
+        app.run('127.0.0.1', port=8109, access_log=True)
     except Exception as e:
         print(e)
