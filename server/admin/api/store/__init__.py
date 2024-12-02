2d80d5629ba8174cc0789541d7363890116bfb94
mirzafar <m_mirzafar@mail.ru>
2024-12-02 07:59:42 +0500

    fix

--- server/admin/api/store/__init__.py
+++ server/admin/api/store/__init__.py
@@ -5,6 +5,7 @@ __all__ = ['store_bp']
 from admin.api.store.categories import StoreCategoriesView, StoreCategoryView
 from admin.api.store.goods import StoreGoodsView, StoreGoodView
 from admin.api.store.overheads import StoreOverheadsView, StoreOverheadView
+from admin.api.store.reasons import StoreReasonsView, StoreReasonView
 
 store_bp = Blueprint('store', url_prefix='/store')
 
@@ -14,3 +15,5 @@ store_bp.add_route(StoreGoodsView.as_view(), '/goods/')
 store_bp.add_route(StoreGoodView.as_view(), '/goods/<good_id>/')
 store_bp.add_route(StoreOverheadsView.as_view(), '/overheads/')
 store_bp.add_route(StoreOverheadView.as_view(), '/overheads/<overhead_id>/')
+store_bp.add_route(StoreReasonsView.as_view(), '/reasons/')
+store_bp.add_route(StoreReasonView.as_view(), '/reasons/<reason_id>/')
--- server/admin/api/store/reasons.py
+++ server/admin/api/store/reasons.py
@@ -1,25 +1,26 @@
 from core.db import db
 from core.handlers import BaseAPIView
+from utils.floats import FloatUtils
 from utils.ints import IntUtils
 from utils.lists import ListUtils
 from utils.strs import StrUtils
 
 
-class StoreCategoriesView(BaseAPIView):
-    template_name = 'admin/store-categories.html'
+class StoreReasonsView(BaseAPIView):
+    template_name = 'admin/store-reasons.html'
 
     async def get(self, request, user):
-        categories = ListUtils.to_list_of_dicts(await db.fetch(
+        reasons = ListUtils.to_list_of_dicts(await db.fetch(
             '''
-            SELECT id, title, unit, description
-            FROM public.categories
+            SELECT id, title, description, price
+            FROM store.reasons
             WHERE is_active
             ORDER BY id DESC
             '''
         ))
 
         return self.success(request=request, user=user, data={
-            'categories': categories
+            'reasons': reasons
         })
 
     async def post(self, request, user):
@@ -27,23 +28,20 @@ class StoreCategoriesView(BaseAPIView):
         if not title:
             return self.error(message='Отсуствует обязательный параметры "Название"')
 
-        unit = StrUtils.to_str(request.json.get('unit'))
-        if not unit:
-            return self.error(message='Отсуствует обязательный параметры "Eдиница измерений"')
-
         description = StrUtils.to_str(request.json.get('description'))
-        if not unit:
-            return self.error(message='Отсуствует обязательный параметры "Eдиница измерений"')
+        price = FloatUtils.to_float(request.json.get('price'))
+        if not price:
+            return self.error(message='Отсуствует обязательный параметры "Цена"')
 
         item = await db.fetchrow(
             '''
-            INSERT INTO public.categories(title, unit, description)
+            INSERT INTO store.reasons(title, description, price)
             VALUES ($1, $2, $3)
             RETURNING *          
             ''',
             title,
-            unit,
-            description
+            description,
+            price
         )
 
         if not item:
@@ -52,51 +50,50 @@ class StoreCategoriesView(BaseAPIView):
         return self.success()
 
 
-class StoreCategoryView(BaseAPIView):
-    async def get(self, request, user, category_id):
-        category_id = IntUtils.to_int(category_id)
-        if not category_id:
+class StoreReasonView(BaseAPIView):
+    async def get(self, request, user, reason_id):
+        reason_id = IntUtils.to_int(reason_id)
+        if not reason_id:
             return self.error(message='Отсуствует обязательный параметры "ID"')
 
-        category = await db.fetchrow(
+        reason = await db.fetchrow(
             '''
-            SELECT title, unit, description
-            FROM public.categories
+            SELECT id, title, description, price
+            FROM store.reasons
             WHERE id = $1
             ''',
-            category_id
+            reason_id
         ) or {}
 
         return self.success(data={
-            'category': dict(category)
+            'reason': dict(reason)
         })
 
-    async def put(self, request, user, category_id):
-        category_id = IntUtils.to_int(category_id)
-        if not category_id:
+    async def put(self, request, user, reason_id):
+        reason_id = IntUtils.to_int(reason_id)
+        if not reason_id:
             return self.error(message='Отсуствует обязательный параметры "ID"')
 
         title = StrUtils.to_str(request.json.get('title'))
         if not title:
             return self.error(message='Отсуствует обязательный параметры "Название"')
 
-        unit = StrUtils.to_str(request.json.get('unit'))
-        if not unit:
-            return self.error(message='Отсуствует обязательный параметры "Eдиница измерений"')
-
         description = StrUtils.to_str(request.json.get('description'))
+        price = FloatUtils.to_float(request.json.get('price'))
+        if not price:
+            return self.error(message='Отсуствует обязательный параметры "Цена"')
 
         item = await db.fetchrow(
             '''
-            UPDATE public.categories
-            SET title = $2, unit = $3, description = $4
+            UPDATE store.reasons
+            SET title = $2, description = $3, price = $4
             WHERE id = $1
             RETURNING *        
             ''',
-            category_id,
+            reason_id,
             title,
-            unit,
-            description
+            description,
+            price
         )
 
         if not item:
@@ -104,19 +101,19 @@ class StoreCategoryView(BaseAPIView):
 
         return self.success()
 
-    async def delete(self, request, user, category_id):
-        category_id = IntUtils.to_int(category_id)
-        if not category_id:
+    async def delete(self, request, user, reason_id):
+        reason_id = IntUtils.to_int(reason_id)
+        if not reason_id:
             return self.error(message='Отсуствует обязательный параметры "ID"')
 
         item = await db.fetchrow(
             '''
-            UPDATE public.categories
+            UPDATE store.reasons
             SET is_active = FALSE
             WHERE id = $1
             RETURNING *        
             ''',
-            category_id
+            reason_id
         )
 
         if not item:
