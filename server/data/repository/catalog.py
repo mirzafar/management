182121eba61a3e87412fc525d929d513f3a0e713
mirzafar <m_mirzafar@mail.ru>
2025-05-19 12:45:36 +0500

    fix

--- server/data/repository/catalog.py
+++ server/data/repository/catalog.py
@@ -5,14 +5,19 @@ import httpx
 from data.repository.goods import ControlGoodsRepository
 from settings import settings
 
-CATALOG_TITLE = (f'1) Хлеб из зеленой гречки без мед\n'
-                 f'2) Хлеб из зеленой гречки с мед (кунжут или семечки можно в комментарии)\n'
-                 f'3) Хлеб из пророшенной пшеницы без мед\n'
-                 f'4) Хлеб Бионан из пророщенной пшеницы с мед (кунжут или семечки можно в комментарии)')
-
 
 async def on_catalog(chat_id: str) -> dict:
     goods = await ControlGoodsRepository.get_goods()
+    print(dict(url=f'{settings["tg_api_url"]}/bot{settings["tg_token"]}/sendMediaGroup',
+               json={
+                   'media': [
+                       {
+                           'type': 'photo',
+                           'media': f'{settings["base_url"]}/static/uploads/{img}'
+                       } for img in goods.values() if img.get('photo')
+                   ],
+                   'chat_id': chat_id}
+               ))
     try:
         async with httpx.AsyncClient(timeout=30) as client:
             await client.post(
